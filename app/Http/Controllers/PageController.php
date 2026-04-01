6b36e8fc8e96cbe43c1d161959bfe1f1d341671e
Nabillatunnafista <nnafista@gmail.com>
2026-04-01 22:08:13 +0700

    update PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,28 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Area; // Tambahkan ini agar data Area bisa diambil
-use App\Models\Team; // Tambahkan ini agar data Team bisa diambil
+use App\Models\Area; 
+use App\Models\Team;
 
 class PageController extends Controller
 {
-    // Ini adalah method 'home' yang dicari Laravel tadi
     public function home() 
     {
-        $areas = Area::all(); // Mengambil data kecamatan dari database
-        return view('home', compact('areas')); // Membuka file home.blade.php
+        $areas = Area::all(); 
+        return view('home', compact('areas')); 
     }
 
-    // Pastikan about juga ada agar tidak error lagi nanti
     public function about() 
     {
-        $team = Team::all(); // Mengambil data tim
-        return view('about', compact('team')); // Membuka file about.blade.php
+        $team = Team::all(); 
+        return view('about', compact('team')); 
     }
 
-    // Dan contact juga
     public function contact() 
     {
-        return view('contact'); // Membuka file contact.blade.php
+        return view('contact'); 
     }
 }
