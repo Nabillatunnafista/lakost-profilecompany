d1c69f1e86abe6f99ab660aafb9ea48a610f1b1b
Nabillatunnafista <nnafista@gmail.com>
2026-04-02 14:13:58 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PageController;
 
-Route::get('/about', [PageController::class, 'about'])->name('about');
-
 Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
